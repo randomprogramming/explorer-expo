7539458a9f6ec1a4a4f95aeb7670270a2fc88ed6
randomprogramming <isnipedodac@gmail.com>
2021-03-20 21:12:52 +0100

    implemented custom bottom tab bar

---
+++ src/components/CustomBottomTabBar.js
@@ -0,0 +1,101 @@
+import React from "react";
+import { StyleSheet, View, TouchableOpacity, Dimensions } from "react-native";
+import useTheme from "../hooks/useTheme";
+import posed from "react-native-pose";
+
+const windowWidth = Dimensions.get("window").width;
+const tabWidth = windowWidth / 5;
+const sliderWidth = tabWidth / 3;
+const SpotLight = posed.View({
+  route0: { x: tabWidth * 0 + sliderWidth },
+  route1: { x: tabWidth * 1 + sliderWidth },
+  route2: { x: tabWidth * 2 + sliderWidth },
+  route3: { x: tabWidth * 3 + sliderWidth },
+  route4: { x: tabWidth * 4 + sliderWidth },
+});
+
+const CustomBottomTabBar = ({ state, descriptors, navigation }) => {
+  const { index: activeRouteIndex } = state;
+  const focusedOptions = descriptors[state.routes[state.index].key].options;
+
+  const theme = useTheme();
+
+  if (focusedOptions.tabBarVisible === false) {
+    return null;
+  }
+
+  return (
+    <View
+      style={[styles.main, { borderTopColor: theme.background.primary[1] }]}
+    >
+      <View style={styles.spotlightContainer}>
+        <SpotLight
+          style={[styles.spotlight, { backgroundColor: theme.accent.primary }]}
+          pose={`route${activeRouteIndex}`}
+        />
+      </View>
+
+      {state.routes.map((route, index) => {
+        let Icon = () => {};
+        if (descriptors[route.key].options.tabBarIcon) {
+          Icon = descriptors[route.key].options.tabBarIcon;
+        }
+
+        const { options } = descriptors[route.key];
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: "tabPress",
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+          }
+        };
+
+        return (
+          <TouchableOpacity
+            accessibilityRole="button"
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            onPress={onPress}
+            style={styles.iconContainer}
+            key={route.key}
+          >
+            <Icon focused={isFocused} />
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+};
+
+export default CustomBottomTabBar;
+
+const styles = StyleSheet.create({
+  main: {
+    flexDirection: "row",
+    height: 58,
+    borderTopWidth: 1,
+  },
+  spotlightContainer: {
+    position: "absolute",
+    bottom: 6,
+    left: 0,
+  },
+  spotlight: {
+    width: sliderWidth,
+    height: 3,
+    borderRadius: 8,
+  },
+  iconContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingBottom: 6,
+  },
+});
--- src/routers/MainRouter/index.js
+++ src/routers/MainRouter/index.js
@@ -1,23 +1,21 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Platform, View } from "react-native";
+import { Platform } from "react-native";
 import * as names from "./names";
 import DiscoverScreen from "../../screens/DiscoverScreen";
 import useTheme from "../../hooks/useTheme";
-import pxGenerator from "../../helpers/pxGenerator";
-import { Entypo } from "@expo/vector-icons";
-import { AntDesign } from "@expo/vector-icons";
 import { Foundation } from "@expo/vector-icons";
 import MapScreen from "../../screens/MapScreen";
 import LikedLocationsScreen from "../../screens/LikedLocationsScreen";
 import SettingsScreen from "../../screens/SettingsScreen";
 import AddLocationRouter from "../AddLocationRouter";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { useSelector } from "react-redux";
+import CustomBottomTabBar from "../../components/CustomBottomTabBar";
+import Icon from "../../components/Icon";
 
 const Tab = createBottomTabNavigator();
 
-const ICON_SIZE = 26;
+const ICON_SIZE = 23;
 
 const MainRouter = () => {
   const isLoggedIn = useSelector((state) => state.person.isLoggedIn);
@@ -29,6 +27,7 @@ const MainRouter = () => {
 
   return (
     <Tab.Navigator
+      tabBar={(props) => <CustomBottomTabBar {...props} />}
       tabBarOptions={{
         tabStyle: {
           backgroundColor: theme.background.primary[0],
@@ -39,48 +38,28 @@ const MainRouter = () => {
       screenOptions={({ route }) => ({
         tabBarIcon: ({ focused, color, size }) => {
           const clr = focused ? theme.accent.primary : theme.accent.secondary;
+
           switch (route.name) {
             case names.DISCOVER_SCREEN:
-              return <Entypo name="home" size={ICON_SIZE} color={clr} />;
+              return <Icon.Home size={ICON_SIZE} color={clr} />;
             case names.MAP_SCREEN:
               return (
-                <Entypo name="location-pin" size={ICON_SIZE} color={clr} />
+                <Icon.Map name="location-pin" size={ICON_SIZE} color={clr} />
               );
             case names.ADD_LOCATION_SCREEN:
-              return <AntDesign name="plus" size={ICON_SIZE} color={clr} />;
+              return <Icon.Plus name="plus" size={ICON_SIZE} color={clr} />;
             case names.LIKED_LOCATIONS_SCREEN:
-              return <Foundation name="heart" size={ICON_SIZE} color={clr} />;
+              return <Icon.Heart name="heart" size={ICON_SIZE} color={clr} />;
             case names.SETTINGS_SCREEN:
               return isLoggedIn ? (
-                // TODO: Use the persons avatar instead of this heart icon
+                // TODO: Chance this with users actual picture
                 <Foundation name="heart" size={ICON_SIZE} color={clr} />
               ) : (
-                <MaterialCommunityIcons
-                  name="account"
-                  size={ICON_SIZE}
-                  color={clr}
-                />
+                <Icon.Avatar name="account" size={ICON_SIZE} color={clr} />
               );
             default:
               console.log("Error");
           }
-        },
-        tabBarLabel: ({ focused, color }) => {
-          return (
-            <View
-              style={{
-                height: 3,
-                width: ICON_SIZE,
-                borderRadius: pxGenerator(5),
-                // If it's focused, give it a color, otherwise make it transparent
-                backgroundColor: focused ? theme.accent.primary : "transparent",
-                position: "relative",
-                top: 0,
-                left: 0,
-                marginBottom: pxGenerator(2),
-              }}
-            ></View>
-          );
         },
       })}
     >
